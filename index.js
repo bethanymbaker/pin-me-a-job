666dd5d0b09717b33efac928888c0444f1f40c87
bethanysimmons <bethanymsimmons@gmail.com>
2013-10-05 13:56:33 -0700

    looks pinnier

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 function drawTiles() {
 	
 	// Mimic JSON file
-	var txt = '{"tiles":[{"src":"images/Grumpy-Cat.jpg","title":"NO","numberofRepins":0,"pinnedFrom":"Gray Ocean Baker"},{"src":"images/Lil-Bub.jpg","title":"YES","numberofRepins":0,"pinnedFrom":"Ronni"}]}';
+	var txt = '{"tiles":[{"src":"images/Grumpy-Cat.jpg","title":"NO","numberofRepins":0,"pinnedFrom":"Gray Ocean Baker"},{"src":"images/Lil-Bub.jpg","title":"YES","numberofRepins":0,"pinnedFrom":"Ronni"},{"src":"images/Nyan-Cat.jpg","title":"Poptart","numberofRepins":0,"pinnedFrom":"Muffin"}]}';
 	var obj = eval("(" + txt + ")");	
 	
 	for (i=0; i<obj.tiles.length; i++) {
 	
 	var canvas = document.createElement('canvas');
+	canvas.style.border = "gray 1px solid";
 	var cat = new Image();
+	cat.title = obj.tiles[i].title;
+	
 	cat.canvas = canvas;
+	cat.canvas.id = i;
 	cat.context = canvas.getContext('2d');
 	cat.onload = function() {
 		return function() {
 			this.canvas.width = this.width;
-			this.canvas.height = this.height;
+			this.canvas.height = this.height + 50;
 			this.context.drawImage(this,0,0);
+			this.context.font = "bold 12px sans-serif";
+			this.context.fillText(this.title,0,this.height+25);
 		};
 	}();
 	cat.src = obj.tiles[i].src;
