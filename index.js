20d77c592be815bb3cd40f37908835037845ce21
bethanysimmons <bethanymsimmons@gmail.com>
2013-10-04 17:35:11 -0700

    add lil-bub plus dynamic canvas

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function drawTile() {
 	
 	//Mimic JSON file
-	var txt = '{"tile":[{"src":"images/Grumpy-Cat.jpg","title":"Grump","numberofRepins":1000,"pinnedFrom":"Gray Ocean Baker"}]}';
+	//var txt = '{"tile":[{"src":"images/Grumpy-Cat.jpg","title":"Grump","numberofRepins":1000,"pinnedFrom":"Gray Ocean Baker"}]}';
+	var txt = '{"tiles":[{"image":"images/Grumpy-Cat.jpg","title":"NO","numberofRepins":0,"pinnedFrom":"Gray Ocean Baker"},{"image":"images/Lil-Bub.jpg","title":"YES","numberofRepins":0,"pinnedFrom":"Ronni"}]}';
 	var obj = eval("(" + txt + ")");
 		
 	var canvas = document.getElementById("b");
@@ -18,3 +19,38 @@ function drawTile() {
 	cat.src = obj.tile[0].src;
 	
 };
+
+function drawTiles() {
+	
+	var myCanvas = document.createElement('canvas');
+	
+	
+	//myCanvas.style.border = "red 1px solid";
+	context = myCanvas.getContext('2d');
+	
+	myCanvas.width = "365";
+	myCanvas.height = "500";
+	
+	var cat = new Image();
+	cat.src = "images/Grumpy-Cat.jpg";
+	cat.onload = function() {
+		context.drawImage(cat,0,0);
+	};
+	
+	/*context = myCanvas.getContext('2d');
+	context.fillStyle = 'red';
+    context.strokeStyle = 'black';
+
+    context.font = '20pt Verdana';
+    context.fillText('Some text', 50, 50);
+    context.strokeText('Some text', 50, 50);
+
+    context.fill();
+    context.stroke();*/
+	
+
+	document.body.appendChild(myCanvas); // adds the canvas to the body element
+	//document.getElementById('someBox').appendChild(canv); // adds the canvas to #someBox
+	
+}
+
