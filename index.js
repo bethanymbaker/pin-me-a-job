8e67dfc84fd56e3563723d3670820d7ad0fa2bf2
bethanysimmons <bethanymsimmons@gmail.com>
2013-10-07 00:02:48 -0700

    refactored

--- index.js
+++ index.js
@@ -4,29 +4,33 @@ function drawTiles() {
 	var txt = '{"tiles":[{"src":"images/Grumpy-Cat.jpg","title":"NO","numberofRepins":0,"pinnedFrom":"Gray Ocean Baker"},{"src":"images/Lil-Bub.jpg","title":"YES","numberofRepins":0,"pinnedFrom":"Ronni"},{"src":"images/Nyan-Cat.jpg","title":"Poptart","numberofRepins":0,"pinnedFrom":"Muffin"}]}';
 	var obj = eval("(" + txt + ")");	
 	
-	for (i=0; i<obj.tiles.length; i++) {
-	
-	var canvas = document.createElement('canvas');
-	canvas.style.border = "gray 1px solid";
-	var cat = new Image();
-	cat.title = obj.tiles[i].title;
-	
-	cat.canvas = canvas;
-	cat.canvas.id = i;
-	cat.context = canvas.getContext('2d');
-	cat.onload = function() {
-		return function() {
-			this.canvas.width = this.width;
-			this.canvas.height = this.height + 50;
-			this.context.drawImage(this,0,0);
-			this.context.font = "bold 12px sans-serif";
-			this.context.fillText(this.title,0,this.height+25);
-		};
-	}();
-	cat.src = obj.tiles[i].src;
-	document.getElementById("myDiv").appendChild(canvas);
-	
+	tilesArray = [];
+	for (i=0; i<obj.tiles.length; i++) {		
+		tilesArray[i] = new Tile(i,obj.tiles[i]);
 	};
 	
+};
 
+function Tile(elementId,info) {
+	this.id = elementId;
+	this.title = info.title;
+	this.numberOfRepins = info.numberOfRepins;
+	this.pinnedFrom = info.pinnedFrom;
+
+	var self = this;
+	this.image = new Image();
+	this.image.onload = function() {
+		return function() {
+		var canvas = document.createElement('canvas');
+		var context = canvas.getContext('2d');
+		canvas.width = this.width;
+		canvas.height = this.height + 50;
+		context.drawImage(this,0,0);
+		context.font = "bold 12px sans-serif";
+		context.fillText(self.title,5,this.height+25);
+		document.getElementById('wrapper').appendChild(canvas);
+		};
+	}();
+	this.image.src = info.src;
+	
 };
